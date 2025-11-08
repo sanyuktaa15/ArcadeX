6fe6ef54955cdb9e4a1cbf304c8a58bee37d3dbc
sanyukta uttam gharde <sugharde_b24@et.vjti.ac.in>
2025-11-08 06:21:41 +0000

    menu page with html css js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('start-btn').addEventListener('click', () => {
+  document.querySelector('.container').style.display = 'none';
+  document.getElementById('game-section').classList.remove('hidden');
+});
